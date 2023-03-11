4613d389d350f7d8751f70b362a0058c83bb4602
Matteo Gheza <matteo.gheza07@gmail.com>
2023-03-11 10:43:09 +0000

    Upload template

---
+++ 01. Template/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+	------------------------------------------------------------------
+	
+	Matteo Gheza
+	------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]) {
+	
+
+	//Definisce la codifica da utilizzare, per permettere di mostrare in output caratteri accentati (UTF-8 e UTF-16)
+	setlocale(LC_ALL, "");
+
+	return 0;
+}
